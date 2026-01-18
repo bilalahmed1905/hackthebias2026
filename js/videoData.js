d55fe5ef7d3cdfe19c10295199432af9bb96ea5d
lolololo82 <leinabdalla7@gmail.com>
2026-01-18 08:09:51 -0700

    added more vids. s
    vids all tested as embedded, all work.

---
+++ js/videoData.js
@@ -0,0 +1,428 @@
+const videoData = [
+      // funny, humor
+{
+        id: 1,
+        videoId: 'IDJOqoNchIQ',
+        title: 'They are not interested in you 😂 #hopecore #hopeless #funny',
+        author: 'HopeleesCore Fuel Up',
+        hashtags: ['funny', 'humor']
+      },
+{
+        id: 2,
+        videoId: 'zU8BLmRzr-4',
+        title: 'bro is def one step ahead 🤣 #fyp #trending #funny #fypシ゚viral',
+        author: 'Yayo Premium ',
+        hashtags: ['funny', 'humor']
+      },
+{
+        id: 3,
+        videoId: 'q-o6NYs8LM8',
+        title: 'Ranking Funniest Rollercoaster Moments😭',
+        author: 'TOP RANKING',
+        hashtags: ['funny', 'humor']
+      },
+{
+        id: 4,
+        videoId: 'eufzhHXphJY',
+        title: 'เเค่นี้ก็เรียบร้อย😱🤣 #shortsfeed  #shorts  #funny  #haha  #fpy  #ตลกฮาฮา #hahaha  #ตลก',
+        author: 'วิถีอีสานบ้านนา',
+        hashtags: ['funny', 'humor']
+      },
+{
+        id: 5,
+        videoId: 'CqnaMjLPPS0',
+        title: '#cute #funny #kpopfunny #kpop #korea',
+        author: 'Percy Nunley',
+        hashtags: ['funny', 'humor']
+      },
+{
+        id: 6,
+        videoId: 'MjSqe5Ta8os',
+        title: '#denzelwashington',
+        author: 'Klick Kitty',
+        hashtags: ['funny', 'humor']      },
+{
+        id: 7,
+        videoId: 'OLf5g9-k9oQ',
+        title: '3 Second Waffle Maker 🤯 #shorts #gadgets #viral',
+        author: '4D Buildss',
+        hashtags: ['funny', 'humor']
+      },
+{
+        id: 8,
+        videoId: '05DcLjNKH8w',
+        title: 'Normal People vs US! 🤣😭 #shorts #funny',
+        author: 'Cadel and Mia',
+        hashtags: ['funny', 'humor']
+      },
+{
+        id: 9,
+        videoId: 'TbS-J16GK6c',
+        title: 'Wait for her 🤣💀',
+        author: 'Eternal Love',
+        hashtags: ['funny', 'humor']      },
+{
+        id: 10,
+        videoId: 'psQaowjXNR4',
+        title: 'POV: Washing Dishes 😭',
+        author: 'The Mannii Show',
+        hashtags: ['funny', 'humor']      },
+{
+        id: 11,
+        videoId: 'hWMnxfC2eRc',
+        title: 'Artist radar 👀 #artistthings #artist #art #artstudent #makingfriends #svanyc #sva',
+        author: 'School of Visual Arts',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 12,
+        videoId: 'sae1U7LZgMA',
+        title: 'It always ends up good somehow #art #drawing #inking #funny',
+        author: 'Ian and Roman',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 13,
+        videoId: 'wyOwu7LPmPE',
+        title: 'Art school is not for the weak 😭 #art',
+        author: 'camileon',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 14,
+        videoId: 'kjYaXuf8OUE',
+        title: 'My secret to taking street photos😎 . #travelphotography #poseideas #fpy #fpyシ #idea #dailylook',
+        author: 'rozes_ss',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 15,
+        videoId: 'shz4wRBfEfs',
+        title: 'Day 8 of TRAUMATIZING MY MATH TEACHER #shorts #creepydrawing #creepy #horror  #analoghorror',
+        author: 'CreepInk',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 16,
+        videoId: '6OPky_5xPzc',
+        title: 'flipbook fight 🤜🤛 #flipbook #remix #flip #art #avee_player #funk #music #shorts',
+        author: 'Manu arts🎨',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 17,
+        videoId: '4N-fT4r-0tw',
+        title: '@wigconicc # #music #sabrinacarpenter #manchild #live #viral #mbf #fyp #xyzbca',
+        author: 'vera (pinkk) 💘',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 18,
+        videoId: 'P3m8QPYK1Uw',
+        title: '🤯 The process of making custom in-ear monitors will blow your mind  #shorts #satisfying #music',
+        author: 'Sam Scott',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 19,
+        videoId: 'ANFDFUSNjAc',
+        title: 'WHICH PART OF THIS VIRAL SONG HIT HARDER (ENGLISH VS. SPANISH) : SHAKIRA "ZOO" #shorts #viral #viral',
+        author: 'Hoover Reaction',
+        hashtags: ['art', 'music', 'photography']
+      },
+{
+        id: 20,
+        videoId: '1KI-XhSZP3A',
+        title: 'When you look up tabs and these are the chords #bandhumor #bandmemes #band #guitar #guitarist',
+        author: 'Sorry Ghost',
+        hashtags: ['art', 'music', 'photography']
+      },
+      {
+        id: 21,
+        videoId: 'bZ_0VPQMYeE',
+        title: 'The Most BIZARRE Conspiracy Theories of 2025',
+        author: 'Popcorn & Plato',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 22,
+        videoId: 'NaVsfWEjhB0',
+        title: 'It took me 9 years to realize this! #disney #disneymovie #movie #conspiracy',
+        author: 'Jack Jerry',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 23,
+        videoId: 'mMUc2SO4NPY',
+        title: 'Labubu\'s may be a sign Theory 😳 #jumpersjump #theory #mystery #shorts #conspiracy #labubu #marketing',
+        author: 'JUMPERS JUMP .',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 24,
+        videoId: '8I42yjrhUK4',
+        title: 'Conspiracy Theories: Flat Earth #shorts',
+        author: 'Captivating History',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 25,
+        videoId: '52fWJGeMGfk',
+        title: 'The Darkest Conspiracy Theories…😳 #shorts #shortsfeed',
+        author: 'STYT',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 26,
+        videoId: 'CluXQYseU3o',
+        title: 'The Conspiracy Theories Are Insane 😳',
+        author: 'Danny Rayes',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 27,
+        videoId: 'tqTfFwWu4Qg',
+        title: 'Ep.40 The Trumps are Time TRAVELERS?? #ninjasarebutterflies #podcast #sundaycool #comedy',
+        author: 'Sunday Cool',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 28,
+        videoId: '3L3ffgrpctE',
+        title: 'Are These Conspiracies Real Or Fake?',
+        author: 'Sambucha',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 29,
+        videoId: 'yGCeDoDbC4w',
+        title: 'every conspiracy theory ends the same | stand up comedy | ramsey badawi',
+        author: 'Ramsey Badawi',
+        hashtags: ['conspiracy']
+      },
+{
+        id: 30,
+        videoId: 'ic3VsVlNxDk',
+        title: '9 HEALTH WARNING SIGNS REVEALED BY LOOKING AT YOUR NAILS! #shorts',
+        author: 'Dr. Mark Leong',
+        hashtags: ['health', 'lifestyle', 'dance']
+      },      
+{
+        id: 31,
+        videoId: '482dSHvdXMA',
+        title: '“we have smoothies at home”',
+        author: 'smoothieflip',
+        hashtags: ['health', 'lifestyle', 'dance']
+      },
+{
+        id: 32,
+        videoId: 'dffDMXV7sGQ',
+        title: 'Eating clean food for 30 days… MIND BLOWING! #cleaneating #weightloss #healthyfood #zest #first',
+        author: 'FirstTasteTips',
+        hashtags: ['health', 'lifestyle', 'dance']
+      },
+{
+        id: 33,
+        videoId: 'iHiV7XRSKuw',
+        title: 'More Protein Less Calories Fast Food Hacks✅',
+        author: 'Flavorful Solutions ',
+        hashtags: ['health', 'lifestyle', 'dance']
+      },
+{
+        id: 34,
+        videoId: 'IhJF6LEHhVs',
+        title: 'Food That Looks Like Your Organs And Heal Them! | Food Is Medicine | Natural Medicine Foods 🌿 #food',
+        author: 'Healthy Foods Doctor',
+        hashtags: ['health', 'lifestyle', 'dance']
+      },
+{
+        id: 35,
+        videoId: 'ArBkPRztJ88',
+        title: 'The 9 Spices That Actually Heal Your Body  #health',
+        author: 'FoodFix',
+        hashtags: ['health', 'lifestyle', 'dance']
+      }, 
+      {
+        id: 36,
+        videoId: 'ad9-jVbcGwQ',
+        title: 'What if we have extra lungs⁉️🤔 #mixtape',
+        author: 'YAN Truth&Giggles',
+        hashtags: ['health', 'lifestyle', 'dance']     },
+{
+        id: 37,
+        videoId: 'LlFS_SFK7T4',
+        title: 'Drop your favorite french song 👇 #confidence #kim #dance',
+        author: 'YoEditz',
+        hashtags: ['health', 'lifestyle', 'dance']      },
+
+{
+        id: 38,
+        videoId: 'MI7jIaLKt_k',
+        title: 'been all over my fyp | #jamesamess',
+        author: 'James',
+        hashtags: ['health', 'lifestyle', 'dance'] }
+{
+        id: 39,
+        videoId: '1p-D_wb3CMo',
+        title: 'How Your Body Reacts During Midnight Scrolling.  #animation  #health',
+        author: 'Dr. Healthcare ',
+        hashtags: ['health', 'lifestyle', 'dance']
+      },
+{
+        id: 40,
+        videoId: '6O-XjSEBnZE',
+        title: 'Body Warning Signs You Should Never Ignore | Health Tips #healthyeating #healthylifestyle',
+        author: 'Fit Food Doctor ',
+        hashtags: ['health', 'lifestyle', 'dance']
+      },
+          
+{
+        id: 41,
+        videoId: 'rEnyxAkyrKE',
+        title: 'How Retractable Staircase Works: Hidden Secret No One Shows You In Japan Living 🤯🔥 #shorts',
+        author: 'CAD COMPANY',
+        hashtags: ['tech', 'business']
+      },
+{
+        id: 42,
+        videoId: 'T2Y2CBaRgho',
+        title: 'These Robot Boots (Shoes) Save You Before You Even Fall. You Wont Believe How 😱 #robot #shorts',
+        author: 'uncover reality',
+        hashtags: ['tech', 'business']      },
+{
+        id: 43,
+        videoId: 'ZL215DZyTkI',
+        title: 'Proving my dad screw screw can get  day 4 🙂 #shorts #tools #tech',
+        author: 'Shresth X',
+        hashtags: ['tech', 'business']      },
+{
+        id: 44,
+        videoId: '3vx2hko0Euk',
+        title: 'Dubai\'s Future Haircut Expo 2050 🤖✂️ The World’s Most Advanced Auto-Cut Machines!',
+        author: 'Adv Life',
+        hashtags: ['tech', 'business']      },
+{
+        id: 45,
+        videoId: 'v2E5fhO4SUM',
+        title: 'First video game',
+        author: 'A2Z',
+        hashtags: ['tech', 'business']      },
+{
+        id: 46,
+        videoId: 'eU_8eoL_UnE',
+        title: 'How this case converts ANY iPhone to USB-C 🤯',
+        author: 'Tech Bits Central',
+        hashtags: ['tech', 'business']      },
+{
+        id: 47,
+        videoId: 'OWMoEcrPtaA',
+        title: 'POV: You\'re closing the Samsung Galaxy Z TriFold wrong',
+        author: 'ZDNET',
+        hashtags: ['tech', 'business']      },
+{
+        id: 48,
+        videoId: 'F2WWa538NJM',
+        title: 'Top 6 IT Jobs in 2026 | Future Tech Careers #shorts',
+        author: 'Simple Know',
+        hashtags: ['tech', 'business']      },
+{
+        id: 49,
+        videoId: 'uwQh3ml3sik',
+        title: 'Making Million-Dollar Business with Plastic Bottles',
+        author: '360° Lives',
+        hashtags: ['tech', 'business']   
+      },
+{
+        id: 50,
+        videoId: 'lAtvhHuPAsQ',
+        title: 'How He Became A Millionaire At 16',
+        author: 'Jordan Welch',
+        hashtags: ['tech', 'business']   
+      },
+      {
+        id: 51,
+        videoId: 'dov0lwJAZ3Q',
+        title: 'China and Canada strike deal to slash tariffs',
+        author: 'CTV News',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+{
+        id: 52, 
+        videoId: 'gAjxxuxA2OQ',
+        title: 'Federal judge: ICE agents cannot detain or tear gas peaceful protesters',
+        author: 'CBC News',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+
+{
+        id: 53, //
+        videoId: 'xH8QdOlyd1E',
+        title: 'Who is the MOST EVIL Person in History? #history #animation',
+        author: 'Historically',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+{
+        id: 54, //
+        videoId: 'CSM6YSkBPi8',
+        title: 'The bathtub of Tsar Alexander #history #art',
+        author: 'History by Mae',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+{
+        id: 55, //
+        videoId: 'KQ5tY1d6Dis',
+        title: 'Evolution of Indonesia #history #geography',
+        author: 'Histomex',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+{
+        id: 56, //
+        videoId: 'SP1Bo0pYK6g',
+        title: 'Washington\'s Mad Mother! - Worst Moms in History #shorts',
+        author: 'Extra History',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+{
+        id: 57,
+        videoId: '0J2K4Y9U1FU',
+        title: 'The Constitution | Recently Deleted History',
+        author: 'Extra History',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+
+
+{
+        id: 58, //
+        videoId: 'hvJYLOe49TA',
+        title: 'Evolution of China #maps #history #historymap',
+        author: 'GeoHistories',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+{
+        id: 59,
+        videoId: 'bJYEgejTc0s',
+        title: 'Suspect accused of setting fires at U of T',
+        author: 'CP24',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+{
+        id: 60,
+        videoId: 'EQ84_nLCF8w',
+        title: 'The most inbred Habsburg. #history #art #painting',
+        author: 'History by Mae',
+        embeddable: true,
+        hashtags: ['news', 'history']
+      },
+      
+];
+
+export default videoData;
